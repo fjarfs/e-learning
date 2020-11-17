cc2f1fbf9637c8fc1b76f27f28d5d19d1c464e2b
Fajar Sujito <fajar@dxtr.asia>
2020-11-17 21:10:06 +0700

    remove assets

--- views/templates/1/1.php
+++ views/templates/1/1.php
@@ -697,18 +697,10 @@
 	<script src="<?php echo $assets;?>/js/jquery-3.2.1.min.js"></script>
 	<script src="<?php echo $assets;?>/js/popper.js"></script>
 	<script src="<?php echo $assets;?>/js/bootstrap.min.js"></script>
-	<!-- <script src="js/stellar.js"></script> -->
 	<script src="<?php echo $assets;?>/js/jquery.magnific-popup.min.js"></script>
 	<script src="<?php echo $assets;?>/vendors/nice-select/js/jquery.nice-select.min.js"></script>
-	<!-- <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script> -->
 	<script src="<?php echo $assets;?>/vendors/isotope/isotope-min.js"></script>
-	<!-- <script src="vendors/owl-carousel/owl.carousel.min.js"></script> -->
 	<script src="<?php echo $assets;?>/js/jquery.ajaxchimp.min.js"></script>
-	<!-- <script src="js/mail-script.js"></script> -->
-	<!--gmaps Js-->
-	<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script> -->
-	<!-- <script src="js/gmaps.min.js"></script> -->
 	<script src="<?php echo $assets;?>/js/theme.js"></script>
 </body>
-
 </html>
